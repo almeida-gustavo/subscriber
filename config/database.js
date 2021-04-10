81d91c472a4a7572530c92fa522a48e6c3c376a6
melogustavo <gustavomelo95@gmail.com>
2021-04-10 10:47:16 -0300

    initial project structure

---
+++ config/database.js
@@ -0,0 +1,18 @@
+require('dotenv/config');
+
+module.exports = {
+  dialect: 'postgres',
+  host: process.env.DB_HOST,
+  username: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  define: {
+    timestamps: true,
+    underscored: true,
+    underscoredAll: true,
+  },
+  timezone: 'America/Sao_Paulo',
+  logging: false,
+  seederStorage: 'sequelize',
+  seederStorageTableName: 'SequelizeData',
+};
